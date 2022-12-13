a0d7b9af7bfbab208d88c3c70fb0f84e93f82740
TitusMagatsela <titus13tito@gmail.com>
2022-12-13 01:21:52 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alpahbet in lowercase
+ *
+ */
+
+void print_aplhabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n');
+}
