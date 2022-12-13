5bae6e070ecc2541523aa45d090bc3ac3b581255
TitusMagatsela <titus13tito@gmail.com>
2022-12-13 05:24:09 -0800

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,14 +14,14 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if
+	else if (n == 0)
 	{
-		_putchar('o');
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
-		_putchar('_');
+		_putchar('-');
 		return (-1);
 	}
 }
