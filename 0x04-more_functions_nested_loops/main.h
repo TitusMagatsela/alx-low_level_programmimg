7a6162cc7f6556b3d332b1dbbbb14e8874309cb6
TitusMagatsela <titus13tito@gmail.com>
2022-12-15 05:42:41 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * file - main.h
+ * Author: Titus Magatsela
+ * Description: Header contains prototypes for all functions 
+ */
+
+int _putchar(char c);
+
+
+
+
+
+
+
+
+
+#endif
