1b97e2094f3e43f1a44380ab71691dbc3290fc1e
TitusMagatsela <titus13tito@gmail.com>
2023-01-09 06:04:01 -0800

    task0

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -2,14 +2,14 @@
 
 /**
  * _isupper - uppercse letters
- * @c: chat to check 
+ * @c: char to be checked
  *
  * Return: 0 or 1
  */
 
 int _isupper(int c)
 {
-	f (c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
