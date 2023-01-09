463e83a5f267a356b06dabbdb359289eb394e5ed
TitusMagatsela <titus13tito@gmail.com>
2023-01-09 04:19:50 -0800

    0-isupper

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercse letters
+ * @c: chat to check 
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	f (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
+
